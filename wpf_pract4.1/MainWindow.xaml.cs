d8ed2d7a47cac0fb78cb191f5612639a0d28d78e
ivan.k <ivan.k@uniweb.ua>
2025-04-21 23:50:51 +0300

    Добавьте файлы проекта.

---
+++ wpf_pract4.1/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_pract4._1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly RoutedCommand CustomCommand = new RoutedCommand();
+
+    private void txtEditor_SelectionChanged(object sender, RoutedEventArgs e)
+    {
+        int row = txtEditor.GetLineIndexFromCharacterIndex(txtEditor.CaretIndex);
+        int col = txtEditor.CaretIndex - txtEditor.GetCharacterIndexFromLineIndex(row);
+
+        lblCursorPosition.Text = $"Рядок {row + 1}, Символ {col + 1}";
+    }
+}
